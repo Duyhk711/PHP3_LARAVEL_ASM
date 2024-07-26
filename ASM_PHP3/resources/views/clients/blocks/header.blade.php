72d84f9d0222e242c241532d8c05590139a3deff
Nhamvuph42227 <nhamvtph42227@fpt.edu.vn>
2024-07-26 23:15:28 +0700

    client

--- ASM_PHP3/resources/views/clients/blocks/header.blade.php
+++ ASM_PHP3/resources/views/clients/blocks/header.blade.php
@@ -1,71 +1,155 @@
-     <div class="container">
-         <nav class="navbar navbar-expand-lg navbar-white">
-             <a class="navbar-brand order-1" href="index.html">
-                 <img class="img-fluid" width="100px" src="{{ asset('assets/clients') }}/images/logo.png" alt="Reader | Hugo Personal Blog Template">
-             </a>
-             <div class="collapse navbar-collapse text-center order-lg-2 order-3" id="navigation">
-                 <ul class="navbar-nav mx-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact.html">Trang Chủ </a>
-                    </li>
+<div class="ltn__header-top-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <div class="ltn__top-bar-menu">
+                    <ul>
+                        <li><a href="locations.html"><i class="icon-placeholder"></i> 15/A, Nest Tower, NYC</a></li>
+                        <li><a href="mailto:info@webmail.com?Subject=Flower%20greetings%20to%20you"><i class="icon-mail"></i> info@webmail.com</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="top-bar-right  text-end">
+                    <div class="ltn__top-bar-menu">
+                        <ul>
+                            <li>
+                                <!-- ltn__language-menu -->
+                                <div class="ltn__drop-menu ltn__currency-menu ltn__language-menu">
+                                    <ul>
+                                        <li><a href="#" class="dropdown-toggle"><span class="active-currency">English</span></a>
+                                            <ul>
+                                                <li><a href="#">Arabic</a></li>
+                                                <li><a href="#">Bengali</a></li>
+                                                <li><a href="#">Chinese</a></li>
+                                                <li><a href="#">English</a></li>
+                                                <li><a href="#">French</a></li>
+                                                <li><a href="#">Hindi</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li>
+                                <!-- ltn__social-media -->
+                                <div class="ltn__social-media">
+                                    <ul>
+                                        <li><a href="#" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#" title="Twitter"><i class="fab fa-twitter"></i></a></li>
+                                        
+                                        <li><a href="#" title="Instagram"><i class="fab fa-instagram"></i></a></li>
+                                        <li><a href="#" title="Dribbble"><i class="fab fa-dribbble"></i></a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ltn__header-top-area end -->
 
-                     <li class="nav-item dropdown">
-                         <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
-                             aria-expanded="false">
-                             Giới Thiệu <i class="ti-angle-down ml-1"></i>
-                         </a>
-                         <div class="dropdown-menu">
-
-                             <a class="dropdown-item" href="about-me.html">About Me</a>
-
-                             <a class="dropdown-item" href="about-us.html">About Us</a>
-
-                         </div>
-                     </li>
-
-                     <li class="nav-item">
-                         <a class="nav-link" href="contact.html">Tin tức</a>
-                     </li>
-
-                     <li class="nav-item dropdown">
-                         <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
-                             aria-expanded="false">Shop<i class="ti-angle-down ml-1"></i>
-                         </a>
-                         <div class="dropdown-menu">
-
-                             <a class="dropdown-item" href="author.html">Bánh Kem</a>
-
-                             <a class="dropdown-item" href="author-single.html">Đồ Ăn Nhanh</a>
-
-                             <a class="dropdown-item" href="advertise.html">Đồ Uống</a>
-
-                             <a class="dropdown-item" href="post-details.html">Đồ Chiên</a>
-                         </div>
-                     </li>
-
-                     <li class="nav-item">
-                        
-                         <a class="nav-link" href="shop.html">Liên Hệ</a>
-                     </li>
-                 </ul>
-             </div>
-
-             <div class="order-2 order-lg-3 d-flex align-items-center">
-                 <select class="m-2 border-0 bg-transparent" id="select-language">
-                     <option id="en" value="" selected>En</option>
-                     <option id="fr" value="">Fr</option>
-                 </select>
-
-                 <!-- search -->
-                 <form class="search-bar">
-                     <input id="search-query" name="s" type="search" placeholder="Type &amp; Hit Enter...">
-                 </form>
-
-                 <button class="navbar-toggler border-0 order-1" type="button" data-toggle="collapse"
-                     data-target="#navigation">
-                     <i class="ti-menu"></i>
-                 </button>
-             </div>
-
-         </nav>
-     </div>
+<!-- ltn__header-middle-area start -->
+<div class="ltn__header-middle-area ltn__header-sticky ltn__sticky-bg-white ltn__logo-right-menu-option sticky-active-into-mobile--- plr--9---">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="site-logo-wrap">
+                    <div class="site-logo">
+                        <a href="index.html"><img src="{{asset('assets/clients')}}/img/logo.png" alt="Logo"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col header-menu-column menu-color-white---">
+                <div class="header-menu d-none d-xl-block">
+                    <nav>
+                        <div class="ltn__main-menu">
+                            <ul>
+                                <li class="menu-icon"><a href="#">Home</a>
+                                  
+                                </li>
+                                <li class="menu-icon"><a href="#">Giới Thiệu</a>
+                                    <ul>
+                                        <li><a href="shop.html">Giới thiệu</a></li>
+                                        <li><a href="shop-grid.html">Địa chỉ</a></li>
+                                        
+                                    </ul>
+                                </li>
+                                <li class="menu-icon"><a href="{{route('shop')}}">Shop</a>
+                                    <ul>
+                                        <li><a href="{{route('shop')}}">Shop</a></li>
+                                        <li><a href="{{route('shop.doUong')}}">Đồ uống</a></li>
+                                        <li><a href="{{route('shop.banhKem')}}">Bánh Kem</a></li>
+                                        <li><a href="{{route('shop.doAnNhanh')}}">Đồ ăn Nhanh</a></li>
+                                        <li><a href="{{route('shop.doChien')}}">Đồ Chiên</a></li>
+                                    </ul>
+                                </li>
+                                <li class="menu-icon"><a href="#">Tin Tức</a>
+                                   
+                                </li>
+                                <li class="menu-icon"><a href="#">Pages</a>
+                                </li>
+                                <li><a href="contact.html">Liên Hệ</a></li>
+                                <li class="special-link"><a href="contact.html">GET A QUOTE</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+            <div class="ltn__header-options ltn__header-options-2">
+                <!-- header-search-1 -->
+                <div class="header-search-wrap">
+                    <div class="header-search-1">
+                        <div class="search-icon">
+                            <i class="icon-search for-search-show"></i>
+                            <i class="icon-cancel  for-search-close"></i>
+                        </div>
+                    </div>
+                    <div class="header-search-1-form">
+                        <form id="#" method="get"  action="#">
+                            <input type="text" name="search" value="" placeholder="Search here..."/>
+                            <button type="submit">
+                                <span><i class="icon-search"></i></span>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                <!-- user-menu -->
+                <div class="ltn__drop-menu user-menu">
+                    <ul>
+                        <li>
+                            <a href="#"><i class="icon-user"></i></a>
+                            <ul>
+                                <li><a href="login.html">Sign in</a></li>
+                                <li><a href="register.html">Register</a></li>
+                                <li><a href="account.html">My Account</a></li>
+                                <li><a href="wishlist.html">Wishlist</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <!-- mini-cart -->
+                <div class="mini-cart-icon">
+                    <a href="{{route('shop.cart')}}" class="ltn__utilize-toggle">
+                        <i class="icon-shopping-cart"></i>
+                        <sup>2</sup>
+                    </a>
+                </div>
+                <!-- mini-cart -->
+                <!-- Mobile Menu Button -->
+                <div class="mobile-menu-toggle d-xl-none">
+                    <a href="#ltn__utilize-mobile-menu" class="ltn__utilize-toggle">
+                        <svg viewBox="0 0 800 600">
+                            <path d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" id="top"></path>
+                            <path d="M300,320 L540,320" id="middle"></path>
+                            <path d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190" id="bottom" transform="translate(480, 320) scale(1, -1) translate(-480, -318) "></path>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ltn__header-middle-area end -->
--- ASM_PHP3/routes/web.php
+++ ASM_PHP3/routes/web.php
@@ -12,23 +12,8 @@ use App\Http\Controllers\Admins\SanPhamController;
 use App\Http\Controllers\Admins\TrangThaiDHController;
 
 use App\Http\Controllers\AuthController;
-
-use App\Http\Controllers\Admins\UserController;
 use App\Http\Controllers\ClientController;
-use App\Http\Controllers\HomeController;
-use Illuminate\Support\Facades\Route;
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Clients\ShopController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -50,19 +35,21 @@ Route::resource('/trangthai',TrangThaiDHController::class);
 
 Route::get('/login', [AuthController::class, 'showFormLogin']);
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-
 Route::get('/register', [AuthController::class, 'showFormRegister']);
 Route::post('/register', [AuthController::class, 'register'])->name('register');
-
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-
 
 
 // Auth::routes();
 
 Route::get('/home', [HomeController::class, 'home'])->name('home');
 Route::get('/dashboard', [HomeController::class, 'index'])->name('index')->middleware(['auth','auth.admin']);
+Route::get('/client/home', [ClientController::class, 'index'])->name('trang_chu');
 
-Route::get('/client/index', [ClientController::class, 'index'])->name('trang_chu');
-
+Route::get('/client/shop',[ShopController::class, 'shop'])->name('shop');
+Route::get('/client/shop/doAnNhanh',[ShopController::class, 'doAnNhanh'])->name('shop.doAnNhanh');
+Route::get('/client/shop/banhKem',[ShopController::class, 'banhKem'])->name('shop.banhKem');
+Route::get('/client/shop/doUong',[ShopController::class, 'doUong'])->name('shop.doUong');
+Route::get('/client/shop/doChien',[ShopController::class, 'doChien'])->name('shop.doChien');
+Route::get('/client/shop/cart',[ShopController::class, 'cart'])->name('shop.cart');
+Route::get('/client/shop/detailProduct',[ShopController::class, 'detailProduct'])->name('shop.detailProduct');
