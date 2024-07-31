e449d0605934d4cf0f7296af447a0b24a80966a5
Nhamvuph42227 <nhamvtph42227@fpt.edu.vn>
2024-07-31 22:58:56 +0700

    conflic file

--- ASM_PHP3/app/Http/Controllers/ClientController.php
+++ ASM_PHP3/app/Http/Controllers/ClientController.php
@@ -19,13 +19,7 @@ class ClientController extends Controller
         // dd($product);
         return view('clients.contents.shops.shop',compact('product'));
     }
-    public function danhmuc(DanhMuc $danhmuc){
-        // $san_phams=SanPham::where('danh_muc_id', $id->id)->get();
-        $sanPham = $danhmuc->sanPhams()->paginate(12);
-
-        // dd($danhmuc->sanPhams);
-        return view('clients.contents.shops.product',compact('danhmuc','sanPham'));
-    }
+   
     public function danhMuc(DanhMuc $cat){
         $sanPham = $cat->sanPhams()->paginate(1);
         // dd($cat->sanPhams);
